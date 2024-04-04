db6d8df6f39a278d75ff755008e0eb545735c18b
At1z <Adrianlistkiewiczv2@gmail.com>
2024-04-04 20:30:32 +0200

    na spokojnie

--- backend/ac_backend/src/main/java/backend/ac_backend/UserController/UserController.java
+++ backend/ac_backend/src/main/java/backend/ac_backend/UserController/UserController.java
@@ -4,6 +4,7 @@ package backend.ac_backend.UserController;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
--- frontend/src/service/UserService.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-const REST_API_BASEURL = 'http://localhost:8080/api/users'
-
-export const listUsers=()=>axios.get(REST_API_BASEURL);
