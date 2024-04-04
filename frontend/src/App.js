50cd16cbd900dacfc3a083faa6c7b8efd89537b1
Maciej <maciekjur123@gmail.com>
2024-04-05 01:03:02 +0200

    kongo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginSignup from './Components/LoginSignup/LoginSignup.jsx';
+import SecuredPage from './Components/Pages/securedPage.js';
 
-function App() {
-  return (
-      <div >
-        <LoginSignup/>
-      </div>
-  );
-}
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LoginSignup />} />
+                <Route path="/api/v1/user" element={<SecuredPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
 export default App;
+
---
+++ frontend/src/Components/Pages/securedPage.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const SecuredPage = () => {
+    return (
+        <div>
+            <h1>Secured Page</h1>
+            <p>This is a secured page accessible only to logged-in users.</p>
+            <p>TKURWA KURWA KURWA KURWA CHUJ CHUJ CHUJ</p>
+        </div>
+    );
+};
+
+export default SecuredPage;
