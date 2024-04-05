6293e244ccde65226c1d9ee989eb5b29dcb34a2a
At1z <Adrianlistkiewiczv2@gmail.com>
2024-04-05 18:12:44 +0200

    cos

--- copy_front/src/App.js
+++ copy_front/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginSignup from './Components/LoginSignup/LoginSignup.jsx';
+import StartingPanel from './Components/StartingPanel/LoginSignup.jsx';
 import SecuredPage from './Components/Pages/securedPage.js';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<LoginSignup />} />
+                <Route path="/" element={<StartingPanel />} />
                 <Route path="/api/v1/user" element={<SecuredPage />} />
             </Routes>
         </BrowserRouter>
